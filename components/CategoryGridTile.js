0df6e3e20249e814a5932df6d0f32016cbdd9ca9
YagoPazos <yagopb@gmail.com>
2025-04-30 12:03:31 +0200

    styling categoryGrid Component

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -1,10 +1,10 @@
-import { Pressable, View, Text } from "react-native";
+import { Pressable, View, Text, StyleSheet, Button } from "react-native";
 
 function CategoryGridTile({ title, color }) {
   return (
-    <View>
-      <Pressable>
-        <View>
+    <View style={styles.gridItem}>
+      <Pressable style={styles.button}>
+        <View style={styles.innerContainer}>
           <Text>{title}</Text>
         </View>
       </Pressable>
@@ -13,3 +13,23 @@ function CategoryGridTile({ title, color }) {
 }
 
 export default CategoryGridTile;
+
+const styles = StyleSheet.create({
+  gridItem: {
+    flex: 1,
+    margin: 16,
+    height: 150,
+    borderRadius: 8,
+    elevation: 4,
+  },
+  button: {
+    flex: 1,
+  },
+  innerContainer: {
+    flex: 1,
+    padding: 16,
+    justifyContent: 'center',
+    alignItems: 'center',
+
+  }
+});
